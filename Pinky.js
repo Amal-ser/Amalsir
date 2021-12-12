c32aad9afe10282478ee2303641d55c464c5f362
Red Alpha <93113261+MhmdMukarram@users.noreply.github.com>
2021-12-12 19:28:33 +0530

    Update Pinky.js

--- Pinky.js
+++ Pinky.js
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+919895828468' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Amalser' : process.env.DEPLOYER,    
+ PHONE: process.env.NUMBER === undefined ? '+94740979220' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Red Alpha' : process.env.DEPLOYER,    
 };
