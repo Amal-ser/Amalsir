1227a35d96367c3822176cbfc39607e1f2c1848c
Red Alpha <93113261+MhmdMukarram@users.noreply.github.com>
2021-12-12 12:01:18 +0530

    Update xlove.js

--- plugins/xlove.js
+++ plugins/xlove.js
@@ -7,23 +7,23 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/w89FHm7.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.ibb.co/Mnc6Gqm/BotLogo.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name: Amalser*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name: Red-Alpha*
 
-*Creator number : wa.me/919895828468?text=Hi%20Amal%20Ser.%20*
+*Creator number : wa.me/94740979220?text=Hi%20Red%20Alpha.%20*
 
  *To check update .update   To update Bot .update now*
 
- *Bot making video : https://youtu.be/Tb1B-rS52uo*
+ *Bot Chat Group : https://chat.whatsapp.com/Jx7f5x3wSU7D8a9ntdFB3j*
      
- *Instagram id: https://www.instagram.com/p/CCdcH3FBd1a/?utm_medium=copy_link*
+ *My Telegram: https://t.me/realredalpha*
 
- *Yt_PASSWORD: _________(password on this video watch it full)*
+ *Yt_PASSWORD: _________(afamk)*
 
- *githublink : https://github.com/Amal-ser/Amalser*
+ *githublink : https://github.com/MhmdMukarram/RED_ALPHA*
 
- *Amalser New Acoount Git Link Old Account Is Flagged Hope you Enjoy This Thanku 🥰*
+ *Red Alpha New Acoount Git Link Old Account Is Flagged Hope you Enjoy This Thanku 🥰*
 `}) 
 
 }));
