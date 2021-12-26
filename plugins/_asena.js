84afd0d02ae8ffc72bc383d0a3b73cb64de32cb6
Amalser <93727572+Amal-ser@users.noreply.github.com>
2021-12-26 11:34:00 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -8,7 +8,7 @@ const Asena = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
 const fs = require("fs")
-const image = require('../raganork');
+const image = require('../buffer');
 const Language = require('../language');
 const Lang = Language.getString('_asena');
 var skcmd, sk1,sk2,sk3,sk4
