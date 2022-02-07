def25ccfb03686a3122213c00d7689fe872eb1bb
AK-JUGGRU <99009188+AK-JUGGRU@users.noreply.github.com>
2022-02-07 12:13:04 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *JUGGRUSER*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *JUGGRUSER*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *JUGGRUSER*:  ```" + msg + "```"
 }
 
 
