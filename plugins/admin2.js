ee9ba3097eea92e8e48830b30cbc593e56e45642
Amalser <93727572+Amal-ser@users.noreply.github.com>
2023-08-28 00:46:11 +0530

    Update admin2.js

--- plugins/admin2.js
+++ plugins/admin2.js
@@ -3,7 +3,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+What's app bot - Amalsir
 */
 
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
